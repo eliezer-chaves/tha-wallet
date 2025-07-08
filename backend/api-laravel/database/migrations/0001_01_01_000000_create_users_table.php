1b34ca6e8d9ae9dd7ac280cfc391aadb4f4dbdb6
eliezer-chaves <chaves.eliezer@outlook.com>
2025-07-08 14:38:35 -0300

    📦 refactor: rename api-tha-wallet directory to api-laravel and reset user migration to default laravel structure

--- backend/api-laravel/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/api-laravel/database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,18 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('user_user', function (Blueprint $table) {
-            $table->id('usr_id');
-            $table->string('usr_first_name', 50);
-            $table->string('usr_last_name', 50);
-            $table->string('usr_email', 100)->unique();
-            $table->string('usr_password');
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
 
